01bd21437d0fb74abec7200b5774f94841255625
lorenamm <lorenamorais@alunos.utfpr.edu.br>
2023-01-23 07:18:25 -0300

    feat: GET transactions

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { login, register } from "../controllers/authController.js";
+import { transactions } from "../controllers/transactionsController.js";
 
 const router = express.Router();
 router.post('/sign-up', register);
 router.post('/', login);
+router.get('/transactions', transactions);
 
 export default router;
